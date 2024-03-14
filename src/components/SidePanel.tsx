c2f1cbb5be92619426b0419f69b82e8c55cd2fc4
fahriedr <fahriramadhan234@gmail.com>
2024-03-14 16:13:13 +0700

    add side panel component

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -13,10 +13,24 @@ const SidePanel = () => {
           </div>
       </div>
       <div className="flex flex-col mt-4 px-2">
-        <FormInput name="search" placeholder="Search"/>
-          body
+        <div className="mb-4">
+            <FormInput name="search" placeholder="Search"/>
+        </div>
+        <div className="flex flex-col">
+            <div className="flex flex-row items-center h-[4rem] cursor-pointer hover:bg-[#0C1317]">
+                <div className="w-1/4">
+                    <Image className="border-solid border rounded-full stroke-black" width={40} height={40} src={'https://robohash.org/borju'} alt=""/>
+                </div>
+                <div className="flex flex-col w-full">
+                    <span className="text-md font-bold">Jonathan</span>
+                    <span className="text-sm">Hello Jon</span>
+                </div>
+                <div className="w-1/4">
+                    <span className="text-xs">14.30</span>
+                </div>
+            </div>
+        </div>
       </div>
-
     </div>
   );
 };
