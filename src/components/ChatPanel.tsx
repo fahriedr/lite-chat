aeaa2f7687c5b9f76885a2dc57bf49568eca0c19
fahriedr <fahriramadhan234@gmail.com>
2024-05-16 16:52:59 +0700

    update pusher socket

--- src/components/ChatPanel.tsx
+++ src/components/ChatPanel.tsx
@@ -7,61 +7,76 @@ import React, { useEffect, useRef, useState } from "react";
 import ChatBubble from "./ChatBubble";
 import ChatInput from "./ChatInput";
 import VerticalDots from "./Icons/VerticalDots";
-import io from 'socket.io-client'
+import io from "socket.io-client";
+import Pusher from "pusher-js";
 
 const ChatPanel = () => {
+  Pusher.logToConsole = true;
 
-  const socket = io('http://localhost:3002', {
-    withCredentials: true,
-    extraHeaders: {
-      "my-custom-header": "abcd"
-    }
-  })
+  const pusher = new Pusher("993f5fb44f2246f24dd7", {
+    cluster: "ap1",
+  });
 
-  const {messages, setMessage, addMessage} = useMessageStore((state) => state)
-  const {conversation} = useConversationStore((state) => state )
+  const channel = pusher.subscribe("my-channel");
 
-  const [message, setMessageValue] = useState('');
+  // const socket = io('http://localhost:3002', {
+  //   withCredentials: true,
+  //   extraHeaders: {
+  //     "my-custom-header": "abcd"
+  //   }
+  // })
 
-  const messagesEndRef = useRef<null | HTMLDivElement>(null)
+  const { messages, setMessage, addMessage } = useMessageStore(
+    (state) => state
+  );
+  const { conversation } = useConversationStore((state) => state);
+
+  const [message, setMessageValue] = useState("");
+
+  const messagesEndRef = useRef<null | HTMLDivElement>(null);
 
   const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
-  }
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   const sendMessage = async (event: any) => {
-    if (event.key === 'Enter') {
-
+    if (event.key === "Enter") {
       const res = await sendMessageApi({
         data: {
           userId: conversation?.friendId!,
-          message: message
-        }
-      })
+          message: message,
+        },
+      });
 
-      setMessageValue('')
+      setMessageValue("");
 
-      const _id = res?.data.data._id as string
-      const newMessage = res?.data.data.message as string
-      const receiverId = res?.data.data.receiverId as string
-      const senderId = res?.data.data.senderId as string
-      const createdAt = res?.data.data.createdAt as string
+      const _id = res?.data.data._id as string;
+      const newMessage = res?.data.data.message as string;
+      const receiverId = res?.data.data.receiverId as string;
+      const senderId = res?.data.data.senderId as string;
+      const createdAt = res?.data.data.createdAt as string;
 
       const data: Message = {
         _id: _id,
         message: newMessage,
         receiverId: receiverId,
         senderId: senderId,
-        createdAt: createdAt
-      }
-      addMessage(data)
-      socket.emit('message', data)
+        createdAt: createdAt,
+      };
+      addMessage(data);
+      // socket.emit('message', data)
     }
-  }
-
+  };
 
   useEffect(() => {
-    scrollToBottom()
+    channel.bind("my-event", function (data: any) {
+      alert(JSON.stringify(data));
+      console.debug(data, 'data');
+    });
+  },[])
+
+  useEffect(() => {
+    scrollToBottom();
   }, [messages]);
 
   return (
@@ -73,10 +88,12 @@ const ChatPanel = () => {
             className="border-solid border rounded-full stroke-black"
             width={50}
             height={50}
-            src={conversation?.friendAvatar ?? 'https://robohash.org/random'}
+            src={conversation?.friendAvatar ?? "https://robohash.org/random"}
             alt=""
           />
-          <span className="pl-[20px] font-semibold text-lg">{conversation?.name}</span>
+          <span className="pl-[20px] font-semibold text-lg">
+            {conversation?.name}
+          </span>
         </div>
         <div className="cursor-pointer">
           <VerticalDots color="white" size={6} />
@@ -85,24 +102,24 @@ const ChatPanel = () => {
 
       {/* Messages */}
       <div className="flex flex-col flex-1 overflow-auto bg-[url('/images/wa-bg.svg')] px-[4px]">
-        {
-          messages.length > 0 ? 
+        {messages.length > 0 ? (
           <>
-            {
-            messages.map((data, i) => {
-                return (
-                  <ChatBubble key={i} data={data}/>
-                )
-              })
-            }
+            {messages.map((data, i) => {
+              return <ChatBubble key={i} data={data} />;
+            })}
           </>
-          : <></>
-        }
+        ) : (
+          <></>
+        )}
         <div ref={messagesEndRef} />
       </div>
 
       {/* Input */}
-      <ChatInput onChange={(e: any) => setMessageValue(e.target.value)} onKeyDown={sendMessage} value={message}/>
+      <ChatInput
+        onChange={(e: any) => setMessageValue(e.target.value)}
+        onKeyDown={sendMessage}
+        value={message}
+      />
     </div>
   );
 };
