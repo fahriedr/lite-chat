19ec965081ed6484e40ac3774419d6e7336f40a5
fahriedr <fahriramadhan234@gmail.com>
2025-03-13 10:53:40 +0700

    update pusher

--- src/store/actions/message.actions.ts
+++ src/store/actions/message.actions.ts
@@ -5,7 +5,7 @@ import { Message } from "@/types"
 
 export const sendMessage = async (message: Message ) => {
     try {
-        pusherServer.trigger('lite-chat', 'upcoming-message', message)
+        await pusherServer.trigger('lite-chat', 'upcoming-message', message)
     } catch (error: any) {
         throw new Error(error.message)
     }
