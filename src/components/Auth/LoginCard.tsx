b2e0e55822766bc8d2bc5aa2f08c0e1832bb036d
fahriedr <fahriramadhan234@gmail.com>
2025-03-13 09:56:17 +0700

    update sign up text

--- src/components/Auth/LoginCard.tsx
+++ src/components/Auth/LoginCard.tsx
@@ -153,7 +153,7 @@ const LoginCard = ({ }: Props) => {
           </form>
           <div className="mt-8 text-center">
             <p className="text-sm text-gray-600">
-              {"Don't have an account?"}
+              {"Dont have an account?"}
               <Link href="/signup" className="font-medium text-blue-600 hover:text-indigo-500">
                 {' '}Sign up
               </Link>
